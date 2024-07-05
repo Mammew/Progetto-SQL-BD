ab30f7be0be2ca45600ad471e392e7983a90bb77
Elena <edeidda03@gmail.com>
2024-07-05 18:43:12 +0200

    file3

--- file_3.sql
+++ file_3.sql
@@ -2,10 +2,10 @@
 --- Numero gruppo
 --- Nomi e matricole componenti
 
-set search_path to 'UniGeSocialSport_p';
+--set search_path to 'UniGeSocialSport_p';
 
 --- PARTE III 
-/* il file deve essere file SQL ... cioè formato solo testo e apribili ed eseguibili in pgAdmin */
+/* il file deve essere file SQL ... cio� formato solo testo e apribili ed eseguibili in pgAdmin */
 
 
 
@@ -20,7 +20,7 @@ CREATE SCHEMA "UniGeSocialSport_p3";
 set search_path to 'UniGeSocialSport_p3';
 
 CREATE TABLE Utente(
-	Username varchar (25) not null, --PRIMARY KEY,
+	Username varchar (25) not null PRIMARY KEY,
 	premium boolean not null DEFAULT false,
 	genere varchar(1) not null, --check(genere in ('M','F')),
 	corso_di_studi varchar (30) NOT NULL,
@@ -32,22 +32,22 @@ CREATE TABLE Utente(
 	affidabile boolean not null default true,
 	matricola decimal (7,0) not null,
 	luogoN varchar(25) not null,
-	dataN timestamp not null --check(dataN < current_date),
-	--UNIQUE (telefono),
-	--UNIQUE (matricola)
+	dataN timestamp not null , --check(dataN < current_date),
+	UNIQUE (telefono),
+	UNIQUE (matricola)
 );
 
 CREATE TABLE Iscrive(
-	Username varchar(25) not null,-- REFERENCES Utente(Username),
+	Username varchar(25) not null REFERENCES Utente(Username),
 	ID decimal(5,0) not null,-- REFERENCES Evento(ID),
-	Sostituto varchar (25),-- REFERENCES Utente(Username) check (user_not_in_this_event(Sostituto, ID)),
-	stato varchar(10),-- check (stato in ('rifiutato','confermato')),
+	Sostituto varchar (25) REFERENCES Utente(Username),-- check (user_not_in_this_event(Sostituto, ID)),
+	stato varchar(10), --check (stato in ('rifiutato','confermato')),
 	data timestamp  not null default current_date,
-	ruolo varchar(10) not null,-- check (ruolo in ('giocatore','arbitro')),
+	ruolo varchar(10) not null ,--check (ruolo in ('giocatore','arbitro')),
 	ritardo boolean,
-	no_show boolean
-	--PRIMARY KEY (Username, ID),
-	--UNIQUE(Username,ID,Sostituto)
+	no_show boolean,
+	PRIMARY KEY (Username, ID),
+	UNIQUE(Username,ID,Sostituto)
 );
 
 
@@ -66,7 +66,7 @@ CREATE TABLE Iscrive(
 /* inserire qui i comandi SQL per la creazione della query, in modo da visualizzarne piane di esecuzione e tempi di esecuzione */ 
 
 EXPLAIN ANALYZE SELECT matricola
-FROM Utente
+FROM UtenteC
 WHERE matricola < 1000000
 
 /*************************************************************************************************************************************************************************/ 
@@ -76,7 +76,7 @@ WHERE matricola < 1000000
 /* inserire qui i comandi SQL per la creazione della query, in modo da visualizzarne piane di esecuzione e tempi di esecuzion */ 
 
 EXPLAIN ANALYZE SELECT matricola
-FROM Utente
+FROM UtenteC
 WHERE luogoN = 'Torino' AND matricola < 5000000
 
 /*************************************************************************************************************************************************************************/ 
@@ -87,8 +87,15 @@ WHERE luogoN = 'Torino' AND matricola < 5000000
 /* inserire qui i comandi SQL per la creazione della query, in modo da visualizzarne piane di esecuzione e tempi di esecuzione */ 
 
 EXPLAIN ANALYZE SELECT DISTINCT u.Username
- FROM Utente u, Iscrive i
+ FROM UtenteC u, IscriveC i
 WHERE stato= 'confermato' AND u.Username = i.Username
+
+EXPLAIN ANALYZE 
+SELECT DISTINCT u.Username
+FROM UtenteC u
+JOIN IscriveC i ON u.Username = i.Username
+WHERE i.stato = 'confermato';
+
 
 /*************************************************************************************************************************************************************************/
 --1e. Schema fisico
@@ -117,38 +124,85 @@ FROM
 WHERE 
     schemaname NOT IN ('pg_catalog', 'information_schema');*/
 	
+SELECT C.oid, relname, relfilenode, relam, relpages, relhasindex, relkind
+FROM pg_namespace N JOIN pg_class C ON N.oid = C.relnamespace
+WHERE N.nspname = 'UniGeSocialSport_p3' 
+
+	
 /* inserire qui i comandi SQL per la creazione dello schema fisico della base di dati in accordo al risultato della fase di progettazione fisica per il carico di lavoro. */
 	
 /*
 Per ogni relazione ci saranno diversi indici ma solo 1 potrà essere clusterizzato
 */
+
+--------------------------------------------------------------
+-- Creazione delle tabelle senza vincoli di chiave primaria o unique
+CREATE TABLE UtenteC AS
+Select *
+from Utente
+WITH DATA;
+
+CREATE TABLE IscriveC AS
+Select *
+from Iscrive
+WITH DATA;
+--------------------------------------------------------------
+-- Non ci sono indici nelle tabelle UtenteC & IscriveC
+/*
+/**********************************************/
+-- indice per la 1° Query. (ma anche per la 2°)
+CREATE INDEX utente_matricola ON UtenteC(matricola ASC);
+/**********************************************/
+-- indice per la 2° Query.
+CREATE INDEX utente_luogon
+ON UtenteC
+USING HASH (luogoN);
+/**********************************************/
+-- inidice per la 3° Query
+CREATE INDEX utente_Username
+ON UtenteC(Username ASC);
+
+CREATE INDEX iscrive_Username
+ON IscriveC(Username ASC);
+/**********************************************/
+
+-- Clusterizzazione degli indici
+CLUSTER UtenteC USING 
+utente_matricola;
+
+CLUSTER IscriveC USING 
+iscrive_username;
+*/
+
+-- Drop degli indici
+/*
 DROP INDEX utente_matricola;
 
 DROP INDEX utente_luogon;
 DROP INDEX utente_username;
 
 DROP INDEX iscrive_username;
-/**********************************************/
--- indice per la 1° Query. (ma anche per la 2°)
-CREATE INDEX utente_matricola ON Utente(matricola ASC);
-/**********************************************/
--- indice per la 2° Query.
-CREATE INDEX utente_luogon
-ON Utente
-USING HASH (luogoN);
-/**********************************************/
--- inidice per la 3° Query
-CREATE INDEX utente_Username
-ON Utente(Username ASC);
+*/
 
-CREATE INDEX iscrive_Username
-ON Iscrive(Username ASC);
+-- chat
+-- Indice sulla colonna di filtro
+CREATE INDEX idx_iscrivec_stato ON IscriveC(stato);
 
-CLUSTER Utente USING 
-utente_matricola;
+-- Indici sulle colonne di join
+CREATE INDEX idx_utentec_username ON UtenteC(Username);
+CREATE INDEX idx_iscrivec_username ON IscriveC(Username);
 
-CLUSTER Iscrive USING 
-iscrive_username;
+-- Indice composto (opzionale)
+CREATE INDEX idx_iscrivec_username_stato ON IscriveC(Username, stato);
+
+CLUSTER IscriveC USING 
+idx_iscrivec_stato;
+
+CLUSTER IscriveC USING 
+idx_iscrivec_username_stato;
+
+
+
 
 /*************************************************************************************************************************************************************************/ 
 --2. Controllo dell'accesso 
@@ -191,11 +245,11 @@ GRANT SELECT, INSERT, UPDATE ON TABLE Impianto TO gestore_impianto;
 --GRANT USAGE, SELECT ON SEQUENCE impianto_id_seq, evento_id_seq TO gestore_impianto;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO utente_semplice;
-GRANT INSERT ON TABLE Iscrive TO utente_semplice; -- Cosi facendo però potrebbe 'confermarsi' da solo = per la candidatura
--- Possibilità GRANT execute ON nomefunzione TO nomeutente --> per fare INSERT in Iscrive senza mettere lo stato = per candidatura
+GRANT INSERT ON TABLE Iscrive TO utente_semplice;
 
 --GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO utente_semplice;
 -- nascondere atributo affidabile per farlo vedere solo all'utente premium
+
 CREATE VIEW Utente_Semplice_View AS
 SELECT
     Username,
